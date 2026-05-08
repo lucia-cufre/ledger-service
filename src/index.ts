a0694824f4edfc46de94132678058d5c3ba4d0be
Lucía Cufré Aman <luchicufre@hotmail.com>
2026-05-08 08:56:59 -0300

    refactor: rename migrationService file to client for clarity

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { createApp } from './app.js';
 import { env } from './config/env.js';
-import { db } from './database/migrationService.js';
+import { db } from './database/client.js';
 import { logger } from './lib/logger.js';
 
 const app = createApp();
